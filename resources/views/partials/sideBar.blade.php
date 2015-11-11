59976764370ad7ac9a29ef7a6dec6e6ab786155d
ken-master <kendelarama@gmail.com>
2015-11-11 15:10:47 +0000

    cleanup sidebar

--- resources/views/partials/sideBar.blade.php
+++ resources/views/partials/sideBar.blade.php
@@ -1,7 +1,7 @@
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
           <!-- Sidebar user panel -->
-          <div class="user-panel">
+<!--           <div class="user-panel">
             <div class="pull-left image">
               <img src="/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
             </div>
@@ -10,16 +10,16 @@
 
               <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
-          </div>
+          </div> -->
           <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
+         <!--  <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
               <input type="text" name="q" class="form-control" placeholder="Search..."/>
               <span class="input-group-btn">
                 <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
               </span>
             </div>
-          </form>
+          </form> -->
           <!-- /.search form -->
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
@@ -38,7 +38,7 @@
                 <li><a href="/product"><i class="fa fa-circle-o"></i>Product</a></li>
               </ul>
             </li>
-            
+<!--             
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-files-o"></i>
@@ -73,7 +73,7 @@
             </li>
 
 
-<!-- 
+
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-laptop"></i>
@@ -164,7 +164,8 @@
             <li class="header">LABELS</li>
             <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
             <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> -->
+            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> 
+-->
           </ul>
         </section>
         <!-- /.sidebar -->
